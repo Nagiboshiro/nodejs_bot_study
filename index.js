a4ee0ee12394b3067768fb7edaab3f28c618541c
Nagiboshiro <vain335@gmail.com>
2022-11-13 15:35:08 +0300

    правки

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const bot = new TelegramApi(token, {polling: true})
 const chats = {}
 
 
-
 const startGame = async (chatId) => {
     await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен ее угадать!`)
     const randomNumber = Math.floor(Math.random() * 10)
@@ -27,40 +26,35 @@ const start = async () => {
         console.log("Подключение к БД сломалость", e)
     }
 
-    bot.setMyCommands([
-        {command: '/start', description: 'Начальное приветствие'},
-        {command: '/info', description: 'Получить информацию о пользователе'},
-        {command: '/game', description: 'Игра угадай цифру'},
-    ])
+    bot.setMyCommands([{command: '/start', description: 'Начальное приветствие'}, {
+        command: '/info',
+        description: 'Получить информацию о пользователе'
+    }, {command: '/game', description: 'Игра угадай цифру'},])
 
     bot.on('message', async msg => {
         const text = msg.text
         const chatId = msg.chat.id
-        
+
         try {
             if (text === '/game') {
                 return startGame(chatId)
             }
 
             if (text === '/start') {
-                const user = await UserModel.findOne({chatId})
-                if (!user) {
-                    await UserModel.create({chatId})
-                }
+                await UserModel.create({chatId})
                 await bot.sendAnimation(chatId, 'https://tlgrm.eu/_/stickers/b0d/85f/b0d85fbf-de1b-4aaf-836c-1cddaa16e002/1.webp')
-                return  bot.sendMessage(chatId, 'Добро пожаловать в сообщество программистов!')
+                return bot.sendMessage(chatId, 'Добро пожаловать в сообщество программистов!')
             }
 
             if (text === '/info') {
                 const user = await UserModel.findOne({chatId})
-                return  bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}, в игре у тебя правильных ответов ${user.right}, неправильных ${user.wrong}`)
+                return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name} ${msg.from.last_name}, в игре у тебя правильных ответов ${user.right}, неправильных ${user.wrong}`)
             }
 
             return bot.sendMessage(chatId, 'Я тебя не понимаю!')
         } catch (e) {
             return bot.sendMessage(chatId, 'Произошла какая то ошибка')
         }
-
 
 
     })
@@ -75,10 +69,10 @@ const start = async () => {
 
         if (data == chats[chatId]) {
             user.right += 1
-             await bot.sendMessage(chatId, `Поздравляю, ты угадал цифру ${chats[chatId]}`, againOptions)
+            await bot.sendMessage(chatId, `Поздравляю, ты угадал цифру ${chats[chatId]}`, againOptions)
         } else {
             user.wrong += 1
-             await bot.sendMessage(chatId, `К сожалению ты даун, я загадал цифру ${chats[chatId]}`, againOptions)
+            await bot.sendMessage(chatId, `К сожалению ты даун, я загадал цифру ${chats[chatId]}`, againOptions)
         }
 
         await user.save()
